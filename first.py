7af793d9c12003b9097d60852b5503b46d8c987d
mkalansari7 <mk.alansari7@gmail.com>
2022-10-22 19:14:38 +0300

    add first.py

---
+++ first.py
@@ -0,0 +1,17 @@
+# Change the following print statement so that it uses the addition operator + and prints out 8
+print(3)
+
+# Write a print statement that uses the division operator / 
+
+# Add a print statement that uses multiplication *
+
+# Create a variable called `length` and assign a value to it
+
+# Create a variable called    `width` and assign a value to it
+
+# Create a variable called `area` and assign to it the multiplication of `length` and `width`
+
+# Print `area` in the following: "The result is " followed by the `area` variable
+
+
+# BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
