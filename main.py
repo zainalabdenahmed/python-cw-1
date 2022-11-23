ccac51d5fe5c8a27dede988fef85ca4fc07ea636
Mohammed Mansour <shalia.kwt@outlook.com>
2022-11-23 19:43:04 +0300

    rename first.py to main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-print("Mohammed Mansour Alajmi")
 # Change the following print statement so that it uses the addition operator + and prints out 8
-print(3 + 5)
 
 # Write a print statement that uses the division operator / 
-print(8 / 2)
+
 # Add a print statement that uses multiplication *
-print(3 * 5)
+
 # Create a variable called `length` and assign a value to it
-length = 15
+
 # Create a variable called    `width` and assign a value to it
-width = 25
+
 # Create a variable called `area` and assign to it the multiplication of `length` and `width`
-area = width * length
+
 # Print `area` in the following: "The result is " followed by the `area` variable
-print(f"The result is {area}")
+
 
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
-print(f"If the length of the rectangle is {length} , and width is {width} Then the area is {area}")
